204dd0d2fa6a3ab91281535f9f05ba96c6a45f5a
Carlos Aguilar <hey@carlos-aguilar.com>
2025-03-22 22:17:12 +0000

    refactor: use snake-case

--- src/ui/file-tree.tsx
+++ src/ui/file-tree.tsx
@@ -9,37 +9,34 @@ interface FileTreeProps {
   className?: string
 }
 
-export const FileTree: FC<FileTreeProps> = ({ root, className }) => {
-  return (
-    <ul className={cn('list-none p-4 text-base', className)}>
-      {root.map(item =>
-        typeof item === 'string' ? (
-          <File key={item} name={item} />
-        ) : (
-          <Directory key={item.name} directory={item} />
-        )
-      )}
-    </ul>
-  )
-}
-
-const File: FC<{ name: string }> = ({ name }) => (
-  <li className='pl-4'>{name}</li>
+export const FileTree: FC<FileTreeProps> = ({ root, className }) => (
+  <ul className={cn('list-none p-4 text-xs', className)}>
+    {root.map(item =>
+      typeof item === 'string' ? (
+        <File key={item} name={item} />
+      ) : (
+        <Directory key={item.name} directory={item} />
+      )
+    )}
+  </ul>
 )
+
+const File: FC<{ name: File }> = ({ name }) => <li className='pl-4'>{name}</li>
 
 const Directory: FC<{ directory: Directory }> = ({ directory }) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <li className='pl-4'>
+      {isOpen ? '📂' : '📁'}{' '}
       <a
-        className='cursor-pointer text-amber-400 decoration-amber-400 hover:underline'
+        className='text-tabi decoration-tabi cursor-pointer hover:underline'
         onClick={() => setIsOpen(!isOpen)}
       >
-        {isOpen ? '📂' : '📁'} {directory.name}
+        {directory.name}
       </a>
       {isOpen && (
-        <ul className='list-none pl-4'>
+        <ul className='list-none'>
           {directory.contents.map((item, index) =>
             typeof item === 'string' ? (
               <File key={index} name={item} />
