f95c05dbcd3b5df124aaa75a67ae7a97ee905dec
Carlos Aguilar <hey@carlos-aguilar.com>
2025-03-22 07:09:39 +0000

    chore: render the file tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import storage from '@/storage'
+import FileTree from '@/ui/FileTree'
 export const dynamic = 'force-dynamic'
 
 export default async function Home({}) {
   const tabs = await storage()
 
-  return (
-    <code>
-      <pre>{JSON.stringify(tabs, null, 2)}</pre>
-    </code>
-  )
+  return <FileTree root={tabs} />
 }
